9592b55f307365f6ce790aca3fe5718b8a3855de
Alexandræ <114509275+Usernamealexandraeisnotavailable@users.noreply.github.com>
2025-12-20 08:32:42 +0100

    Create index.php

---
+++ graphemathique/index.php
@@ -0,0 +1,588 @@
+<title>Graphémathique (working title)</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Andika:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
+<style>
+td, input, span {
+	font-family: Andika
+}
+td {
+    font-size : 100pt
+}
+input[type="checkbox"] {
+	width: 30px;
+	height: 30px
+}
+</style>
+<table width="100%" height="100%">
+<tr width="100%" height="100%">
+<td width="100%" height="100%" valign="center">
+<center>
+<?php
+
+$fonctions = [];
+$ecriture = [];
+
+function espacements_en_trois ($str) {
+	$retour = "";
+	$j = 0;
+	for ($i = strlen($str)-1; $i >= 0; $i--) {
+		$retour .= $str[$i];
+		$j++;
+		if ($j % 3 == 0 and $i != 0)
+			$retour .= ",\\";
+	}
+	return strrev($retour);
+}
+
+// SIGNE / NOMBRES RELATIFS
+
+function rel_alea () {
+    if (rand(0,1))
+        return "-";
+    return "";
+}
+
+// ENTIERS
+
+function ent_alea () {
+    $fini = false;
+    $retour = strval(rand(1,9));
+    while (!$fini) {
+        $retour .= strval(rand(0,9));
+        $fini = rand(0,1); // proba 1/2
+    }
+    return espacements_en_trois($retour);
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'ent_alea',
+	"prerequis" => [
+		"dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['ent_alea'] = "dec";
+function short_ent_alea () {
+    $fini = false;
+    $retour = strval(rand(1,9));
+    while (!$fini) {
+        $retour .= strval(rand(0,9));
+        $fini = 1 - rand(0,1)*rand(0,1); // proba 3/4
+    }
+    return $retour;
+}
+
+// FRACTIONS
+
+function denom_alea () {
+    $retour_int = 1;
+    $fini = false; 
+    while (!$fini) {
+        $retour_int *= 2;
+        $fini = rand(0,1); // proba 1/2
+    }
+    $fini = false; 
+    while (!$fini) {
+        $retour_int *= 5;
+        $fini = 1-rand(0,1)*rand(0,1); // proba 3/4
+    }
+    return $retour_int;
+}
+function entfrac_alea () {
+	$multi = 1;
+	if (rand(0,1)*rand(0,1)) // proba 1/4
+		$multi = 3;
+	$denom = denom_alea()*$multi;
+    return "\\frac{".espacements_en_trois(strval(intval(short_ent_alea())*$denom))."}{".espacements_en_trois(strval($denom))."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entfrac_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		"frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entfrac_alea'] = "frac";
+function frac_alea () {
+	$multi = 1;
+	if (rand(0,1)*rand(0,1)) // proba 1/4
+		$multi = 3;
+    return "\\frac{".espacements_en_trois(strval(intval(str_replace("\\,","",ent_alea()))*$multi))."}{".espacements_en_trois(strval(denom_alea()*$multi))."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'frac_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		"frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['frac_alea'] = "frac";
+
+// ÉCRITURES DÉCIMALES
+
+function dec_alea () {
+	$fini = rand(0,1);
+	$zeros = "";
+	while (!$fini) {
+		$zeros .= "0";
+		$fini = rand(0,1);
+	}
+	if (rand(0,1))
+		return ent_alea()."{,}".$zeros.short_ent_alea();
+	return "0{,}".$zeros.short_ent_alea();
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'dec_alea',
+	"prerequis" => [
+		"dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['dec_alea'] = "dec";
+
+// ÉCRITURE SCIENTIFIQUE
+// chiffres significatifs : nombre de chiffres apparents dans une écriture scientifique du nombre
+
+function entsci_alea () {
+    return rand(1,9)."\\times10^{".rand(0,9)."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entsci_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		"sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entsci_alea'] = "sci";
+function entrelsci_alea () {
+    return rel_alea().rand(1,9)."\\times10^{".rel_alea().rand(1,9)."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entrelsci_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		"sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entrelsci_alea'] = "sci";
+function decsci_alea () {
+    return rand(1,9)."{,}".short_ent_alea()."\\times10^{".rand(0,9)."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'decsci_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		"sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		// "rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['decsci_alea'] = "sci";
+function decrelsci_alea () {
+    return rel_alea().rand(1,9)."{,}".short_ent_alea()."\\times10^{".rel_alea().rand(1,9)."}";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'decrelsci_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		"sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['decrelsci_alea'] = "sci";
+
+// FONCTIONS COMBINÉES
+
+function entrel_alea () {
+	return rel_alea().ent_alea();
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entrel_alea',
+	"prerequis" => [
+		"dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entrel_alea'] = "dec";
+function decrel_alea () {
+	return rel_alea().dec_alea();
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'decrel_alea',
+	"prerequis" => [
+		"dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['decrel_alea'] = "dec";
+function fracrel_alea () {
+	return rel_alea().frac_alea();
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'fracrel_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		"frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['fracrel_alea'] = "frac";
+function entfracrel_alea () {
+	return rel_alea().entfrac_alea();
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entfracrel_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		"frac", // Fractions
+		// "sci", // Écriture scientifique
+		// "pc", // Pourcentage
+		"rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entfracrel_alea'] = "frac";
+function entpc_alea () {
+	return espacements_en_trois(strval(intval(short_ent_alea())*100))."\\,\\%";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entpc_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		"pc", // Pourcentage
+		// "rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entpc_alea'] = "pc";
+function entrelpc_alea () {
+	return rel_alea().espacements_en_trois(strval(intval(short_ent_alea())*100))."\\,\\%";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'entrelpc_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		"pc", // Pourcentage
+		"rel", // Relatifs
+		// "nonent", // Non-entières
+	]
+);
+$ecriture['entrelpc_alea'] = "pc";
+function decpc_alea () {
+	return dec_alea()."\\,\\%";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'decpc_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		"pc", // Pourcentage
+		// "rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['decpc_alea'] = "pc";
+function decrelpc_alea () {
+	return rel_alea().dec_alea()."\\,\\%";
+}
+$fonctions[count($fonctions)] = array (
+	"nom" => 'decrelpc_alea',
+	"prerequis" => [
+		// "dec", // Écriture décimale
+		// "frac", // Fractions
+		// "sci", // Écriture scientifique
+		"pc", // Pourcentage
+		"rel", // Relatifs
+		"nonent", // Non-entières
+	]
+);
+$ecriture['decrelpc_alea'] = "pc";
+
+
+
+// MENU
+
+if (!isset($_GET["ok"])) {
+?>
+
+<form method="get"><br>
+<table><tr>
+<td style="font-size: 30pt">
+<label><input type="checkbox" name="dec" checked> Écriture décimale</label><br>
+<label><input type="checkbox" name="frac" checked> Fractions</label><br>
+<label><input type="checkbox" name="sci" checked> Écriture scientifique</label><br>
+<label><input type="checkbox" name="pc" checked> Pourcentage</label><br><small><small>
+<label><input type="checkbox" name="rel" checked style="width: 18pt; height: 18pt"> <i>Relatifs</i></label><br>
+<label><input type="checkbox" name="nonent" checked style="width: 18pt; height: 18pt"> <i>Valeurs non-entières</i></label><br>
+<input type="submit" name="ok" value="OK" style="font-size: 30pt; width: 100%">
+</table>
+</form>
+
+<?php
+} else {
+	
+	// QUESTIONS
+	
+	if (isset($_GET["nonent"]))
+	$specifiques = array (
+		"dec" => [
+			"<li>Quelle est sa partie entière&nbsp;?",
+			"<li>Quelle est sa partie décimale&nbsp;?",
+			"<li>Quel est son chiffre des unités&nbsp;?",
+			"<li>Donnez-en un arrondi à l'unité près."
+		],
+		"frac" => [
+			"<li>Quel est son numérateur&nbsp;?",
+			"<li>Quel est son dénominateur&nbsp;?"
+		],
+		"sci" => [
+			"<li>Quel est son ordre de grandeur&nbsp;?",
+			"<li>Quel est son exposant&nbsp;?",
+			"<li>Combien de chiffres significatifs&nbsp;?"
+		],
+		"pc" => []
+	);
+	else
+	$specifiques = array (
+		"dec" => [
+			"<li>Quelle est sa partie entière&nbsp;?",
+			"<li>Quel est son chiffre des unités&nbsp;?"
+		],
+		"frac" => [
+			"<li>Quel est son numérateur&nbsp;?",
+			"<li>Quel est son dénominateur&nbsp;?"
+		],
+		"sci" => [
+			"<li>Quel est son ordre de grandeur&nbsp;?",
+			"<li>Quel est son exposant&nbsp;?"
+		],
+		"pc" => []
+	);
+	
+	$conversions = array (
+		array (
+			"schema" => "<li>Donnez-en une écriture décimale.\n",
+			"prerequis" => [
+				"dec", // Écriture décimale
+				// "frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				// "rel", // Relatifs
+				// "nonent", // Non-entières
+			],
+			"redondance" => "dec"
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture décimale, avec au moins ".rand(2,5)." chiffres après la virgule.\n",
+			"prerequis" => [
+				"dec", // Écriture décimale
+				// "frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				// "rel", // Relatifs
+				"nonent", // Non-entières /* techniquement ça resterait des entiers, mais les virgules ne sont pas vus avant */
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture comme fraction qui n'est pas une fraction décimale.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				"frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				// "rel", // Relatifs
+				// "nonent", // Non-entières
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture comme fraction décimale.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				"frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				// "rel", // Relatifs
+				// "nonent", // Non-entières
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture comme fraction avec un numérateur négatif.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				"frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				"rel", // Relatifs
+				// "nonent", // Non-entières
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture comme fraction avec un dénominateur négatif.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				"frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				"rel", // Relatifs
+				// "nonent", // Non-entières
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture scientifique.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				// "frac", // Fractions
+				"sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				"rel", // Relatifs /* ça risque de demander des magnitudes négatives */
+				"nonent", // Non-entières /* en général ça risque de demander des nombres non-entiers */
+			],
+			"redondance" => "sci"
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture scientifique, avec au moins ".rand(2,5)." chiffres significatifs.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				// "frac", // Fractions
+				"sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				"rel", // Relatifs /* ça risque de demander des magnitudes négatives */
+				"nonent", // Non-entières /* en général ça risque de demander des nombres non-entiers, ou avec virgule explicite */
+			]
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture en tant que pourcentage.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				// "frac", // Fractions
+				// "sci", // Écriture scientifique
+				"pc", // Pourcentage
+				// "rel", // Relatifs
+				// "nonent", // Non-entières
+			],
+			"redondance" => "pc"
+		),
+		array (
+			"schema" => "<li>Donnez-en une écriture qui commence par un signe moins.\n",
+			"prerequis" => [
+				// "dec", // Écriture décimale
+				// "frac", // Fractions
+				// "sci", // Écriture scientifique
+				// "pc", // Pourcentage
+				"rel", // Relatifs
+				// "nonent", // Non-entières
+			]
+		)
+	);
+	
+	// JEU
+	
+	function fonctions_utilisables () {
+		global $fonctions;
+		$retour = [];
+		foreach ($fonctions as $fonction) {
+			$bool = true;
+			foreach ($fonction["prerequis"] as $pre) {
+				if (!isset($_GET[$pre])) {
+					$bool = false;
+				}
+			}
+			if ($bool)
+				$retour[count($retour)] = $fonction["nom"];
+		}
+		return $retour;
+	}
+	
+	function conversions_utilisables ($ecriture) {
+		global $conversions;
+		$retour = [];
+		foreach ($conversions as $conversion) {
+			$bool = true;
+			if (isset($conversion["redondance"]))
+				if ($conversion["redondance"] == $ecriture)
+					$bool = false;
+			foreach ($conversion["prerequis"] as $pre) {
+				if (!isset($_GET[$pre])) {
+					$bool = false;
+				}
+			}
+			if ($bool)
+				$retour[count($retour)] = $conversion["schema"];
+		}
+		return $retour;
+	}
+	
+	$utilisables = fonctions_utilisables();
+	
+	if (count($utilisables) == 0) {
+		print "<i style='font-size: 30pt'>hein&nbsp;?</i>";
+		print "<meta http-equiv='refresh' content='1; ?'>";
+	} else {
+		$fun = $utilisables[rand(0,count($utilisables)-1)];
+		print "<br>\n<span style='color: white'>";
+		print "\\(\\color{black}\\sf".$fun()."\\)";
+		print "</span><table><tr><td><ol style='font-size: 20pt'><br><br>";
+		$questions = [];
+		foreach ($specifiques[$ecriture[$fun]] as $specifique)
+			$questions[count($questions)] = $specifique."\n";
+		foreach (conversions_utilisables($ecriture[$fun]) as $conversion)
+			$questions[count($questions)] = $conversion."\n";
+		shuffle ($questions);
+		$nombre_restants = 6;
+		foreach ($questions as $question) {
+			print $question;
+			$nombre_restants--;
+			if ($nombre_restants == 0)
+				break;
+		}
+		print "<br><a href='' style='font-size: 50pt; color: rgb(50,100,255); text-decoration: none'>⟳</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='?' style='font-size: 50pt; color: rgb(50,100,255); text-decoration: none'>⌫</a></table>";
+	}
+	
+}
+?>
