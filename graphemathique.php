7686f402ca55d5b3e922c11b367afaec1b261a45
Alexandræ <114509275+Usernamealexandraeisnotavailable@users.noreply.github.com>
2026-01-06 22:08:20 +0100

    Update graphemathique.php

--- graphemathique.php
+++ graphemathique.php
@@ -1,4 +1,10 @@
 <title>Graphémathique</title>
+<?php
+foreach ($_GET as $i => $v) {
+	$_GET[$i] = str_replace(["\"","<",">"],["&quot;","&lt;","&gt;"],$v);
+	// petite sécurité lol
+}
+?>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Andika:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
